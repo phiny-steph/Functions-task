c9e193dceabefbc10567a90a13d5cec42418c9d0
phiny-steph <86403160+phiny-steph@users.noreply.github.com>
2021-11-03 02:24:56 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+1. Declare a function called laugh() that returns "hahahahahahahahahaha!". Print the value returned from the laugh() function to the console.
+/*
+* Programming Quiz: Laugh it Off 1
+*/
+
+/*
+* QUIZ REQUIREMENTS
+* - Your code should have a `laugh()` function
+* - Your `laugh()` function should return the correct output
+* - Your code should print `\"hahahahahahahahahaha!\"` by calling the `laugh()` function inside `console.log()`
+* - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+*/
+
+// your code goes here
+
+console.log(/* call the laugh function */);
+
