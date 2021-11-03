c80ec3492b6712388329dd07c009342993f59def
phiny-steph <86403160+phiny-steph@users.noreply.github.com>
2021-11-03 03:10:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,46 @@
 
 // your code goes here
 
-console.log(/* call the laugh function */);
+function laugh(){
+  return "hahahahahahahahahaha";
+}
+console.log(laugh());
+
+
+
+/*
+2. Write a function called laugh() that takes one parameter, num that represents the number of "ha"s to return.
+TIP: You might need a loop to solve this!
+Here's an example of the output and how to call the function that you will write:
+console.log(laugh(3));
+Prints: "hahaha!"
+/*
+* Programming Quiz: Laugh it Off 2
+*
+* Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+*
+* Note:
+*  - make sure your the final character is an exclamation mark ("!")
+*  - make sure that your function produces the correct results when it is called multiple times
+*/
+
+/*
+* QUIZ REQUIREMENTS
+* - Your code should have a `laugh()` function
+* - Your `laugh()` function should have one parameter named `num`
+* - Your `laugh()` function should return the correct number of laughs
+*/
+
+
+function laugh (num){
+  let laughHa  = "";
+    for (let i =0; i<num; i++){
+      
+      laughHa = laughHa + "ha";
+    }
+  }
+  return laughHa + "!";
+}
+console.log(laugh(10));
+
 
